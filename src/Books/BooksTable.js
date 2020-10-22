4846a64016a4aa944080f31c1707d686f9f306dc
monika.m <monika.m@thoughtworks.com>
2020-10-22 13:45:06 +0530

    Add. Show table view of the books

--- src/Books/BooksTable.js
+++ src/Books/BooksTable.js
@@ -1,12 +1,25 @@
 import React from "react";
+
 export default function BooksTable(props) {
-    return <div>
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Title</th>
-                <th>Price</th>
-            </tr>
-        </table>
-    </div>
+    const books = props.books;
+    return <table>
+        <thead>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+            books.map((book) => {
+                return <tr>
+                    <td>{book.title}</td>
+                    <td>{book.author}</td>
+                    <td>{book.price}</td>
+                </tr>
+            })
+        }
+        </tbody>
+    </table>
 }
--- src/Books/BooksTable.test.js
+++ src/Books/BooksTable.test.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import {render} from '@testing-library/react'
 import BooksTable from "./BooksTable";
 
 describe('BooksTable', () => {
     it('should display only headers when there are no rows', function () {
-        const books = [{name: "Malcom Gladwell", title: "Outliers", price: 200}];
-        const { getByText } = render(<BooksTable books={books}/>);
+        const { getByText } = render(<BooksTable books={[{}]}/>);
 
-        expect(getByText('Name')).toBeTruthy();
         expect(getByText('Title')).toBeTruthy();
+        expect(getByText('Author')).toBeTruthy();
         expect(getByText('Price')).toBeTruthy();
     });
+
+    it('should display single book when there is a book', function () {
+        const { getByText } = render(<BooksTable books={[books()[0]]}/>);
+
+        expect(getByText('Malcom Gladwell')).toBeTruthy();
+        expect(getByText('Outliers')).toBeTruthy();
+        expect(getByText('200')).toBeTruthy();
+    });
+
+    it('should display multiple rowhen there is a book', function () {
+        const { getByText } = render(<BooksTable books={books()}/>);
+
+        expect(getByText('Malcom Gladwell')).toBeTruthy();
+        expect(getByText('J K Rowling')).toBeTruthy();
+    });
+
+    function books() {
+        return [{title: "Malcom Gladwell", author: "Outliers", price: 200},
+            {title: "J K Rowling", author: "Harry Potter", price: 500}];
+    }
 })
