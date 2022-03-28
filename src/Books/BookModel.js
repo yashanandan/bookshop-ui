b0282567709ca27a19f2170d8f5f9b8a892fd5fa
monika <monikamahanthappa@gmail.com>
2022-03-28 11:09:06 +0530

    Fix tests

--- src/Books/BookModel.js
+++ src/Books/BookModel.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default class BookModel {
-    static fetchAll() {
-        return axios.get('http://localhost:8080/books', this.authHeaders());
+    static fetchAll = async () => {
+        const response = await axios.get('http://localhost:8080/books', this.authHeaders());
+        return response.data;
     }
 
     static authHeaders() {
