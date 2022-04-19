64064ff56ec974dc2ab8679e7bbcf65544818cbc
monika <monikamahanthappa@gmail.com>
2022-04-19 17:12:27 +0530

    Add. Test environment variables

--- src/Books/BookModel.js
+++ src/Books/BookModel.js
@@ -11,7 +11,7 @@ export default class BookModel {
 
     static fetchAll = async (bookOrAuthorName = '') => {
         const env = runtimeEnv();
-        console.log('env ', env)
+        console.log('env ', env.REACT_BOOK_SHOP)
         const baseUrl = env.REACT_BOOK_SHOP || 'http://localhost:8080';
         console.log('baseUrl ', baseUrl);
         const url = `${baseUrl}/api/books?bookOrAuthorName=${bookOrAuthorName}`;
--- src/Books/BookModel.test.js
+++ src/Books/BookModel.test.js
@@ -5,10 +5,10 @@ import axios from 'axios';
 jest.mock('axios');
 
 describe('BookModel', () => {
-    xit('should fetch all books from api',  async () => {
+    it('should fetch all books from api',  async () => {
         axios.get = jest.fn().mockResolvedValue({data: booksFactory()});
         const books = await BookModel.fetchAll();
-        const url = "http://localhost:8080/api/books?bookOrAuthorName=";
+        const url = "http://someurl/api/books?bookOrAuthorName=";
         const headers = {"auth": {"password": "test", "username": "test2@book.com"}};
         expect(axios.get).toHaveBeenCalledWith(url, headers);
         expect(books).toHaveLength(2);
