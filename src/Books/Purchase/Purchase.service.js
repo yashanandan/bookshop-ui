d569290b2a8cc007c8592b150e38c4cf426acc6a
premtw <prem.manokaran@thoughtworks.com>
2022-04-19 18:18:18 +0530

    Fix the book order api

--- src/Books/Purchase/Purchase.service.js
+++ src/Books/Purchase/Purchase.service.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 export default class PurchaseService {
     static orderBook = async (orderDetails) => {
-        const baseUrl = process.env.REACT_APP_BOOK_SERVICE_URL;
-        const url = `${baseUrl}/orders`;
+        const env = runtimeEnv();
+        const baseUrl = env.REACT_APP_BOOK_SERVICE_URL;
+        const url = `${baseUrl}/api/orders`;
         const result = await axios.post(url, orderDetails, this.authHeaders());
         return result.data;
     }
