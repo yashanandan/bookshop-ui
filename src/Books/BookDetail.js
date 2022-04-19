2e645f7e2e120364e0a0769f3b3b554ef833c10d
shaiknazneen <nazneen.shaik@thoughtworks.com>
2022-04-19 13:11:26 +0530

    removed unused variable

--- src/Books/BookDetail.js
+++ src/Books/BookDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function BookDetail() {
     let { id } = useParams();
